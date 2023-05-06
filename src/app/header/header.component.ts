e43c331572f644709d323604ee0ecb58c437df35
Илья Безруков <bezrukovila200212@gmail.com>
2023-05-07 00:13:11 +0500

    header: Added device detector

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,8 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {}
+export class HeaderComponent implements OnInit {
+  desktop = true;
+
+  ngOnInit(): void {
+    const device = new RegExp(
+      'Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini',
+      'i'
+    );
+    if (device.test(navigator.userAgent)) {
+      this.desktop = false;
+    }
+  }
+}
